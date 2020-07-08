7777e8932708cb2478b5ecd244634bf0386dec92
juchu168 <qihuo99@users.noreply.github.com>
2020-07-08 16:57:34 +0800

    Add Location Index show records function and some bootstrap files

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -6,15 +6,23 @@
                 <h1>All Locations:</h1>
                 <hr />
                 
+                <table border = "1">
+                    <tr>
+                        <td>Id</td>
+                        <td>Location</td>
+                        <td>Note</td>
+                        <td>CreateDate</td>
+                        <td>View Details</td>
+                    </tr>
+
                 @foreach ($locations as $location)
-                    <div class="well">
-                        <h3>{{ $location->location }} </h3>
-                        <p>
-                            {{ $location->note}}
-                        </p>
-                        <a href="{{ route('locations.show', $location->id) }}" class="btn">View Details</a>
-                    </div>
-                    <hr />
+                    <tr>
+                        <td>{{ $location->id }}</td>
+                        <td>{{ $location->location }}</td>
+                        <td>{{ $location->note }}</td>
+                        <td>{{ $location->created_at}}</td>
+                        <td><a href="{{ route('locations.show', $location->id) }}" class="btn">View Details</a></td>
+                    </tr>
                 @endforeach
 
                 {!! $locations->links() !!}  {{-- this is for adding pagination function --}}
