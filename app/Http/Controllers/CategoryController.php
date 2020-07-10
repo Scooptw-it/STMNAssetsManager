24b44ba0e85fd2acba276ec4c63ec271aa907103
juchu168 <qihuo99@users.noreply.github.com>
2020-07-10 16:28:59 +0800

    Add the delete function in Category Controller destroy() function.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -120,6 +120,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = Category::findOrFail($id); //In case the id is not found
+        $category->delete();
+
+        return redirect()->route('categories.index')->with('message', 'Category Deleted.');
     }
 }
